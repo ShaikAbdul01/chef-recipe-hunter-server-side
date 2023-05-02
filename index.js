194fcb54a75b4d3ab26646e44817af1496b5915e
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-02 14:20:43 +0600

    chef details json created

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express=require('express')
-const cors=require('cors')
-const app=express()
-const port=process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
-const chefs=require("./data/chefs.json")
-app.use(cors())
-app.get('/',(req,res)=>{
-    res.send('Server is running')
-})
-app.get('/chefs',(req,res)=>{
-    res.send(chefs)
-})
-
-app.listen(port,()=>{
-    console.log(`API is running on port : ${port}`);
-})
+const chefs = require("./data/chefs.json");
+const chefDetails = require("./data/chefDetails.json");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+app.get("/details", (req, res) => {
+  res.send(chefDetails);
+});
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selecteId = chefDetails.find((detail) => detail.chef.id == id);
+  res.send(selecteId);
+});
+app.listen(port, () => {
+  console.log(`API is running on port : ${port}`);
+});
